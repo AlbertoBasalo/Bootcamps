681a651710cf0e6eb7702897b2013dc9374660a9
Alberto Basalo <albertobasalo@hotmail.com>
2021-09-09 16:48:52 +0200

    feat: branding

--- src/app/shared/data/bootcamps.service.ts
+++ src/app/shared/data/bootcamps.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -9,6 +9,8 @@ import { environment } from 'src/environments/environment';
 export class BootcampsService {
   constructor(private http: HttpClient) {}
   getBootcamps$(): Observable<Record<string, string>[]> {
-    return this.http.get<Record<string, string>[]>(`${environment.apiHost}`);
+    return this.http
+      .get<Record<string, string>[]>(`${environment.apiHost}`)
+      .pipe(map((list) => list.filter((i) => i.Name.length > 0)));
   }
 }
