543d5a4498fe8471166a8b4377366461def8c95f
Alberto Basalo <albertobasalo@hotmail.com>
2021-09-10 15:02:19 +0200

    feat: basic sort and filter

--- src/app/domain/home/bootcamps/bootcamps.component.ts
+++ src/app/domain/home/bootcamps/bootcamps.component.ts
@@ -6,8 +6,48 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./bootcamps.component.scss'],
 })
 export class BootcampsComponent implements OnInit {
-  @Input() bootcamps: Record<string, string>[] | null = null;
+  private original: Record<string, string>[] | undefined;
+  private lastSortKey: string = '';
+  @Input() bootcamps: Record<string, string>[] | undefined;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.original = [...(this.bootcamps || [])];
+    this.sort('');
+  }
+
+  public searchByText(text: string) {
+    this.bootcamps = this.original?.filter((b) => {
+      return JSON.stringify(b)
+        .toLocaleLowerCase()
+        .includes(text.trim().toLocaleLowerCase());
+    });
+    if (this.bootcamps?.length === 0) {
+      this.bootcamps = this.original;
+    }
+    this.sort(this.lastSortKey);
+  }
+
+  public sort(key: string) {
+    this.lastSortKey = key;
+    if (key === 'price') {
+      this.sortByPrice();
+    } else if (key === 'tags') {
+      this.sortByTag();
+    }
+  }
+
+  private sortByPrice() {
+    this.bootcamps = this.bootcamps?.sort((a, b) => {
+      return (
+        Number.parseFloat(b.Price || '0') - Number.parseFloat(a.Price || '0')
+      );
+    });
+  }
+
+  private sortByTag() {
+    this.bootcamps = this.bootcamps?.sort((a, b) => {
+      return (b.Tags || '') > (a.Tags || '') ? 1 : -1;
+    });
+  }
 }
